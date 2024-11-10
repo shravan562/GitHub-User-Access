92c72c2060aef47e169beb50be9e0776a42a4851
Siddhant Khare <Siddhantkhare2694@gmail.com>
2024-11-10 10:06:51 +0000

    Remove unused useEffect import from App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import './App.css';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
